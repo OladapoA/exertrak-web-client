f9ee0d0fcb9c6182e0dc42440f55e50f62ece552
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2020-12-30 21:20:03 +0000

    Addition of muscle group name dependent on route

--- src/views/Exercises.js
+++ src/views/Exercises.js
@@ -9,23 +9,28 @@ function Exercises({match}) {
     },[match])
 
     const [exercises, setExercises] = useState([]);
+    const [muscleGroup, setmuscleGroup] = useState({});
 
     const fetchItems = async () => {
         if (match.params.id){
             // console.log(match.params.id)
             const exercises = await fetch(`/api/v1/muscle_groups/${match.params.id}/exercises`);
+            const muscleGroup = await fetch(`/api/v1/muscle_groups/${match.params.id}`);
             const exercisesData = await exercises.json();
+            const muscleGroupData = await muscleGroup.json();
             setExercises(exercisesData)
+            setmuscleGroup(muscleGroupData)
         } else {
             const exercises = await fetch("/api/v1/exercises/");
             const exercisesData = await exercises.json();
             setExercises(exercisesData)
+            setmuscleGroup({})
         }
     }
 
     return(
         <div>
-            <h1>EXERCISES</h1>
+            {typeof muscleGroup.major_name === "undefined" ? <h1>ALL EXERCISES</h1> : <h1>{muscleGroup.major_name} Exercises</h1>}
             <div>
                 {(exercises.length > 0) &&
                     exercises.map(exercise => <Exercise key={exercise.exercise_id} exercise={exercise} />)
