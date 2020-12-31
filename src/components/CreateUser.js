1d908ebc1d78f9b82a25c786a58f1147a2768df8
Oladapo Awoniyi <oladapoawoniyi@gmail.com>
2020-12-31 16:49:26 +0000

    modal + other formatting

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function Example() {
+function CreateUser() {
     const [show, setShow] = useState(false);
   
     const handleClose = () => setShow(false);
@@ -11,25 +11,25 @@ function Example() {
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
+                Create a new user
             </Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>New ExerTrak user</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
                 <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Create
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
     );
 }
   
-export default Example;
+export default CreateUser;
--- src/views/Users.js
+++ src/views/Users.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import User from "../components/User"
-import Example from "../components/Example"
+import CreateUser from "../components/CreateUser"
 
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -28,33 +28,13 @@ function Users({match}) {
     
     return(
         <div>
-            <h1>USERS</h1>
+            <h1>Users</h1>
             <div>
                 {(users.length > 0) &&
                     users.map(user => <User key={user.user_id} user={user} />)
                 }
             </div>
-            <Example/>
-            <>
-                <Button variant="primary" onClick={handleShow}>
-                    Launch demo modal
-                </Button>
-
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                    </Modal.Footer>
-                </Modal>
-            </>
+            <CreateUser />
         </div>
     )
 }
